9355c4d102799261adf090838fe158c4e87d1178
pingson <b6p31079@shonan.bunkyo.ac.jp>
2019-09-23 12:02:47 +0900

    wordpressへのアップロード容量設定 追記 (10Gへ増量)

--- wordpress-nginx_rhel8/roles/wordpress/templates/wp-config.php
+++ wordpress-nginx_rhel8/roles/wordpress/templates/wp-config.php
@@ -88,3 +88,10 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+/** wordpress へのアップロード容量 */
+@ini_set( 'upload_max_filesize' , '10G' );
+@ini_set( 'post_max_size', '10G');
+@ini_set( 'memory_limit', '512M' );
+@ini_set( 'max_execution_time', '300' );
+@ini_set( 'max_input_time', '300' );
